cbb62fa93a50c6aa89119479a311230ae4f35fef
Aashir Siddiqui <aashirsiddiqui217@gmail.com>
2025-03-14 06:07:28 +0500

    Update website

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 // Hiding Container-1
 document.querySelector(".close-logo").addEventListener(("click"), () => {
     document.querySelector(".cont-1").style.display = "none"
-    document.querySelector(".cont-2").style.margin = "0px"
+    document.querySelector(".cont-2").style.top = "0"
 
 })
 
 // Auto Typing
 new Typed("#autowriting", {
-    strings: ["discounts on your first order!!", "free Delivery on your first order!!"],
+    strings: ["discounts on your first order!", "free delivery on your first order!"],
     typeSpeed: 100,
     backSpeed: 100,
     loop: true
